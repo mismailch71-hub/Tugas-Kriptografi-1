27cad4d6f1b83381adeafaf0d5b4bed73b4ecbd9
m. ismail cahyadi <m.ismail.ch71@gmail.com>
2026-09-17 07:59:48 +0800

    push

--- Kriptografi.py
+++ Kriptografi.py
@@ -1,5 +1,5 @@
 def decrypt_enigma(ciphertext):
-    # Konfigurasi Rotor (Standard Enigma I)
+
     rotors = {
         'I': 'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
         'II': 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
@@ -8,21 +8,16 @@ def decrypt_enigma(ciphertext):
     reflector_b = 'YRUHQSLDPXNGOKMIEBFZCWVJAT'
     turnovers = {'I': 'Q', 'II': 'E', 'III': 'V'}
 
-    # Setting Kanan ke Kiri (I, II, III)
-    # Posisi awal (kanan ke kiri): E, B, M -> (4, 1, 12)
     pos = [4, 1, 12] 
-    
-    # Ring setting (kanan ke kiri): F, U, T -> (5, 20, 19)
+ 
     rings = [5, 20, 19]
 
-    # Plugboard
     plugboard = {'A':'U', 'U':'A', 'D':'L', 'L':'D'}
 
     def step_rotors():
         step_mid = False
         step_left = False
-        
-        # Enigma double-stepping mechanism
+
         if chr(pos[1] + 65) == turnovers['II']:
             step_mid = True
             step_left = True
@@ -38,12 +33,10 @@ def decrypt_enigma(ciphertext):
     plaintext = ""
     for char in ciphertext:
         step_rotors()
-        
-        # Melewati Plugboard
+
         c = plugboard.get(char, char)
         c_idx = ord(c) - 65
 
-        # Sinyal Maju: Kanan (I) -> Tengah (II) -> Kiri (III)
         for i in range(3):
             offset = (pos[i] - rings[i]) % 26
             in_idx = (c_idx + offset) % 26
@@ -55,10 +48,8 @@ def decrypt_enigma(ciphertext):
             out_char = wiring[in_idx]
             c_idx = (ord(out_char) - 65 - offset) % 26
 
-        # Reflector B
         c_idx = ord(reflector_b[c_idx]) - 65
 
-        # Sinyal Mundur: Kiri (III) -> Tengah (II) -> Kanan (I)
         for i in range(2, -1, -1):
             offset = (pos[i] - rings[i]) % 26
             in_idx = (c_idx + offset) % 26
@@ -71,13 +62,11 @@ def decrypt_enigma(ciphertext):
             out_idx = wiring.index(in_char)
             c_idx = (out_idx - offset) % 26
 
-        # Melewati Plugboard (Kembali)
         out_char = chr(c_idx + 65)
         out_char = plugboard.get(out_char, out_char)
         plaintext += out_char
         
     return plaintext
 
-# Jalankan fungsi
 ciphertext = "CVVFFOKLVNYOGVECAPBVPALVSLKUMXQAISLAAM"
 print("Plaintext Bagian 3:", decrypt_enigma(ciphertext))
